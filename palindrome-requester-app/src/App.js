2596609f48bc1879f6d5fd0ebef064c018d53fe7
Elvio <elviobg@gmail.com>
2018-03-05 23:40:49 -0300

    Inserts textfield on web app
    Insert textfield and ake link betwen inserted text and send button to verify if is a palindrome

--- palindrome-requester-app/src/App.js
+++ palindrome-requester-app/src/App.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {
-    response: ''
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      response: '',
+      word: ''  
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({word: event.target.value});
+  }
 
   componentDidMount() {
     this.callApi()
@@ -12,18 +22,34 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
+  setStateAnswer = function(response){
+    if (response.status !== 200) 
+      console.log(this.state.word.concat(" is a palindrome"))
+    else
+      console.log(this.state.word.concat(" is not a palindrome"))
+  }
+
   callApi = async () => {
-    const response = await fetch('/palindrome/palindrome').then(response => console.log(response.status));
-    //if (response.status !== 200) throw Error(body.message);
-    
+    console.log(this.state.word)
+    const response = await fetch('/palindrome/'.concat(this.state.word))
+    const json = await response.json();
+    this.setStateAnswer(json);
   };
 
   render () {
     return (
-      <div className='button__container'>
-        <button className='button' onClick={this.callApi}>
-          Click Me!
-        </button>
+      <div>
+        <div className='label__container'>
+          <label>
+            <textarea value={this.state.word} onChange={this.handleChange}/>
+          </label>
+        </div>
+        <br/>
+        <div className='button__container'>
+          <button className='button' onClick={this.callApi}>
+            Click Me!
+          </button>
+        </div>
       </div>
     )
   }
